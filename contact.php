e6af2255528db814cfe4adc6fb8990f009a173a2
FrostDog <aleks.gubskyy@gmail.com>
2019-08-30 18:50:45 -0400

    Aboutme deisgn
    Need to finish - contact form

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <?php
 
 // Put contacting email here
-$php_main_email = "afiguta.email";
+$php_main_email = "aleks.gubskyy@gmail.com";
 
 //Fetching Values from URL
 $php_name = $_POST['ajax_name'];
